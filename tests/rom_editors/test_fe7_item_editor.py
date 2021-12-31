bb943e12a7324880deda2d1b09a18f1adb18981f
David Greeson <swdrummer13@gmail.com>
2021-12-31 15:27:40 -0500

    Fixed bug with S rank weapons
     - Armads, Durandal, Ereshkigal, Forblaze and Sol Katti could be used by anyone
     - Now they're weapon levels are set to S rank

--- tests/rom_editors/test_fe7_item_editor.py
+++ tests/rom_editors/test_fe7_item_editor.py
@@ -10,10 +10,11 @@ from rom_editors.fe7.item_editor import FE7ItemEditor
 @pytest.fixture(name="item_edit")
 def create_item_editor(rom_data, game_config):
     """ Create an item editor object with FE7 specific items for testing """
-    game_config["items"]["prf"] = [0]
+    game_config["items"]["prf"] = {"e": [0], "s": [1]}
     game_config["items"]["offsets"]["ability3"] = 7
+    game_config["items"]["offsets"]["rank"] = 8
 
-    game_config["items"]["flux_weapon_lvl_pos"] = 8
+    game_config["items"]["flux_weapon_lvl_pos"] = 9
     game_config["items"]["s"] = {"dark": [2]}
     game_config["classes"]["character_stats"]["final_bosses"] = ["boss1"]
     game_config["classes"]["characters"] = {"boss1": {"s_rank_locations": [9]}}
@@ -24,6 +25,7 @@ def test_handle_prf(item_edit):
     """ Test the handle_prf method """
     item_edit.handle_prf()
     assert item_edit.rom_data[7] == 0
+    assert item_edit.rom_data[9] == 251
 
 
 def test_handle_s_rank(item_edit):
@@ -31,5 +33,4 @@ def test_handle_s_rank(item_edit):
     with mock.patch.object(item_edit, "_get_item_type", return_value="dark"):
         item_edit.handle_s_rank()
 
-    assert item_edit.rom_data[8] == 1
     assert item_edit.rom_data[9] == 2
